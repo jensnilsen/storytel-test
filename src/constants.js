3a9fa90c09af7b23fc959c045be19e7473f14f58
Jens_Nilsen <jensnilsen89@gmail.com>
2020-05-11 00:47:40 +0200

    local server url

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 // url for deployed server
-// export const URL = 'https://storytel-test.herokuapp.com/'
+export const URL = 'https://storytel-test.herokuapp.com/'
 
 // url for local server
-export const URL = 'http://localhost:8080/'
+// export const URL = 'http://localhost:8080/'
 
 export const STATUS = {
   SUCCESS: 'SUCCESS',
